421d2a585d1ce688f105fb6cd7653643d2ed53b5
Yaroslav <yaroslav.smswarriors@gmail.com>
2018-12-06 00:05:38 +0100

    - stands selectable
    - filtering list of stands

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ let app = (function () {
 
     let svg = document.getElementById('Layer_1');
     let svg$ = $('#Layer_1');
-
     let textElement = document.getElementsByTagName('text');
     let textLength = textElement.length;
+    let rectElement = document.getElementsByTagName('rect');
 
     function _setViewBox(viewBoxWidth, viewBoxHeight) {
         //svg.setAttribute("viewBox", viewBoxX + ' ' + viewBoxY + ' ' + viewBoxWidth + ' ' + viewBoxHeight);
@@ -22,24 +22,33 @@ let app = (function () {
         for (let i = 0; i < textLength; i++) {
             let id = textElement[i].textContent;
             textElement[i].id = id;
-            textElement[i].addEventListener('click', () => {
 
-                let element = document.getElementById(id);
+            //add 'click' event listeners to text elements
+            textElement[i].addEventListener('click', (e) => {
 
-                element.scrollIntoView({
-                    block: 'start',
-                    behavior: 'smooth'
-                });
+                let target = svg.getElementById('rect-' + id);
 
+                _selectStand(target);
             });
+
+            //add 'click' event listeners to rect elements
+            if (rectElement['rect-' + i]) {
+                rectElement['rect-' + i].addEventListener('click', (e) => {
+
+                    let target = e.target;
+
+                    _selectStand(target);
+                });
+            }
         }
     }
 
-    function _addBtnEventListeners() {
+    function _addEventListeners() {
 
         let zoominBtn = document.getElementById('zoomin');
         let zoomoutBtn = document.getElementById('zoomout');
         let zoomclearBtn = document.getElementById('zoomclear');
+        let searchBtn = document.getElementById('search-btn');
 
         zoominBtn.addEventListener('click', () => {
             //let currentWidth = svg.getAttribute('viewBox').split(" ")[2];
@@ -47,15 +56,37 @@ let app = (function () {
             //_setViewBox(+currentWidth + 100, +currentHight + 50);
             _zoomIn();
         });
+
         zoomoutBtn.addEventListener('click', () => {
             //let currentWidth = svg.getAttribute('viewBox').split(" ")[2];
             //let currentHight = svg.getAttribute('viewBox').split(" ")[3];
             //_setViewBox(+currentWidth - 100, +currentHight - 50);
             _zoomOut();
         });
+
         zoomclearBtn.addEventListener('click', () => {
             let currentWidth = svg.style.width;
             svg.style.width = '100%';
+            svg.style.left = '0px';
+            svg.style.top = '0px';
+        });
+
+        searchBtn.addEventListener('click', () => {
+            let serchInput = document.getElementById('searchInput');
+            let listOfStends = document.getElementById('list-of-stands');
+
+            if (!serchInput.value) {
+                listOfStends.childNodes.forEach(e => e.style.display = 'block');
+            } else {
+                listOfStends.childNodes.forEach(e => {
+                    if (e.innerHTML.indexOf(serchInput.value) != -1) {
+                        e.style.display = 'block';
+
+                    } else {
+                        e.style.display = 'none';
+                    }
+                });
+            }
         });
 
         svg$.bind('mousewheel', function (e) {
@@ -77,10 +108,18 @@ let app = (function () {
         let lines = '';
 
         for (let i = 0; i < textLength; i++) {
-            lines += '<li class="list-group-item">' + textElement[i].textContent + '</li>';
+            let id = textElement[i].textContent;
+            lines += '<li class="list-group-item" name="list-options" id="' + id + '">' + id + '</li>';
         }
 
         stands.innerHTML = lines;
+
+        let listOptions = document.getElementsByName('list-options');
+        listOptions.forEach(e => {
+            e.addEventListener('click', () => {
+                _selectStand(document.getElementById('rect-' + e.id))
+            })
+        });
     }
 
     function _zoomIn() {
@@ -93,20 +132,37 @@ let app = (function () {
         svg.style.width = +currentWidth.slice(0, -1) + 20 + '%';
     }
 
+    function _selectStand(target) {
+
+        if (!target) return;
+
+        let activeElements = svg.getElementsByClassName('stand-active');
+        if (activeElements.length > 0) {
+            activeElements[0].classList.remove("stand-active");
+        }
+
+        target.classList.add('stand-active');
+
+        target.scrollIntoView({
+            block: 'end',
+            behavior: 'smooth'
+        });
+    }
+
     return {
         makeDraggable: _makeDraggable,
         setViewBox: _setViewBox,
         addIDsForAllTextElements: _addIDsForAllTextElements,
-        addBtnEventListeners: _addBtnEventListeners,
-        populateListOfStands: _populateListOfStands
+        addEventListeners: _addEventListeners,
+        populateListOfStands: _populateListOfStands,
     }
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    //document.getElementsByClassName('sidebar')[0].style.display = 'none';
     app.setViewBox(1255, 595.3);
     app.addIDsForAllTextElements();
-    app.addBtnEventListeners();
+    app.addEventListeners();
     app.makeDraggable();
     app.populateListOfStands();
 
